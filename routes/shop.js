6728cde225694aa19cc279475ce7753167c5be51
msdom96 <sagdom96@gmail.com>
2024-03-28 15:31:20 +0100

    fully fixd

--- routes/shop.js
+++ routes/shop.js
@@ -10,5 +10,13 @@ router.get('/', (req, res, next) => {
     res.sendFile(path.join(rootDir,'views', 'shop.html'));
 }); 
 
+router.post('/add-product', (req, res, next) => {
+    // Process the form data here if needed
+    console.log(req.body);
+
+    // Redirect to the root URL '/'
+    res.redirect('/');
+});
+
 module.exports = router;
  
