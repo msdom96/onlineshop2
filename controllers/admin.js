b7e4a27f6a505437461069d8539d57afdcbf3868
msdom96 <sagdom96@gmail.com>
2024-04-09 18:45:44 +0200

    more pages

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('add-product', {
+    res.render('admin/add-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
         formsCSS: true,
