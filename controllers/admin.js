9ac03a1d4d238d2004f003ead87a37ba6de9a1f3
msdom96 <sagdom96@gmail.com>
2024-04-10 15:09:01 +0200

    new routes

--- controllers/admin.js
+++ controllers/admin.js
@@ -11,7 +11,12 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-    const product = new Product(req.body.title);
+    const title = req.body.title;
+    const imgUrl = req.body.imageUrl;
+    const price = req.body.price;
+    const description = req.body.description;
+
+    const product = new Product(title, imgUrl, price, description);
     product.save();
     res.redirect('/');
 };
--- routes/shop.js
+++ routes/shop.js
@@ -10,9 +10,11 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts); 
 
-router.get('/cart', shopController.getCart); 
+router.get('/cart', shopController.getCart);
 
-router.get('/checkout', shopController.getCheckout); 
+router.get('/orders', shopController.getOrders); 
+
+router.get('/checkout', shopController.getOrders); 
 
 
 
